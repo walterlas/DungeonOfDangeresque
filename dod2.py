f08f620ae3b15e04a7f86d4bc2f9b36038880de3
walterlas <walter-las@hotmail.com>
2020-03-28 16:24:42 -0500

    Argh

--- dod2.py
+++ dod2.py
@@ -19,6 +19,7 @@ gameloop= True
 
 difficulty	= 1
 maxlevels	= 10
+columns		= 9
 
 def yesNo():
 	print("")
@@ -63,6 +64,113 @@ def findFleeExit():
 			else:
 				continue
 	return
+
+def getDifficulty():			#Line 5530
+	difficulty	= 0
+	while (difficulty < 1) or (difficulty > 2):
+		print("Difficulty Level: 1 = Moderate, 2 = Hard")
+		n = input("Enter difficulty level > ")
+		difficulty = int(n)
+	return(n)
+
+def getLevels():
+	deep	= 0
+	while deep == 0:
+		levels=input("How many levels deep do you want to start? ")
+		levels=int(levels)
+		if (levels >0) and (levels <= 5):
+			deep = 1
+	return(levels)
+
+def getIndex(x,y):
+	i=x+(columns*y)
+	return(i)
+
+def createGrid(x,y):
+	grid=[]
+	for i in range(0,x*y):
+		grid.append(i)
+	return(grid)
+
+#def fillArray(col,row):
+#	grid	= createGrid(col,row)
+#	n		= 0
+#	for y in range(1,col):
+#		for x in range(1,row):
+#			i = getIndex(x,y,col)
+#			grid[i] = int(rnd()*7+1)	# Returns 1-7
+#	
+#	h	= int(rnd()*3+1)				# Returns 1-3
+#	
+#	for n in range(1,h+1):
+#		x	= int(rnd()*col)
+#		y	= int(rnd()*row)
+#		i	= getIndex(x,y,col)
+#		grid[i]	= 8		# Trap doors?
+#		
+#	s	= int(rnd()*4+1)+2
+#
+#	for n in range(1,s+1):
+#		x	= int(rnd()*8+1)
+#		y	= int(rnd()*8+1)
+#		i	= getIndex(x,y,col)
+#		grid[i]	= 9		# Stairs Up?
+#	return(grid)
+	
+def fillMap(lvl):
+	global level
+
+#	seed()
+	n	= 0
+	for y in range(1,9):
+		for x in range(1,9):
+#			i = level[lvl].getIndex(x,y)
+			i = getIndex(x,y)
+			level[lvl] = int(rnd()*7+1)
+		
+	h	= int(rnd()*3+1)
+	
+	for n in range(1,h+1):
+		x	= int(rnd()*9)
+		y	= int(rnd()*9)
+		i	= getIndex(x,y)
+		level[lvl]	= 8
+		
+	s	= int(rnd()*4+1)+2
+		
+	for n in range(1,s+1):
+		x	= int(rnd()*8+1)
+		y	= int(rnd()*8+1)
+		i	= getIndex(x,y)
+		level[lvl]	= 9
+	return
+	
+def getName():
+	n = input("Enter your character's name > ")
+	o = str(n)
+	return(o)
+
+def monsterSetup():
+	info=[[],[],[]]
+	info[0].append(0)
+	info[1].append(0)
+	info[2].append(0)
+	monster=['Large Dragon','Hideous Ghoul','Lizard Man','Manticore','Purple Worm','Deadly Cobra',
+			'Mad Elf','Clay Man','Hairy Beast','Mad Dwarf','Zombie','Berserker','Giant Scorpion',
+			'Giant Cockroach','Doppleganger','Giant Fire Beetle','Giant Ant','Giant Tick',
+			'Mummy','Nasty Orc','Skeleton','Troll','Goblin','Vampire Bat','Creeping Blob',
+			'Mad Dog','Large Spider','Black Cat','Man Eating Plant','Hydra','Gelatinous Cube',
+			'Giant Centipede','Giant Rat','Shadow']
+	monsterhp=[6,5,4,6,6,5,5,4,5,4,4,5,6,4,5,1,1,2,3,2,1,3,3,3,3,2,3,2,1,3,2,1,2,2]
+	monsterhm=[12,10,8,12,12,10,10,8,10,8,8,10,12,8,10,2,2,4,6,4,2,6,6,6,6,4,6,4,2,6,4,2,4,4]
+	monsterName=" "
+	monsterHitPower=0
+	monsterStrength=0
+	for loop in range(0,34):
+		info[0].append(monster[loop])
+		info[1].append(monsterhp[loop])
+		info[2].append(monsterhm[loop])
+	return(info)
 	
 def	intoPit():
 	global pitfall
@@ -244,7 +352,7 @@ def playerDead():
 	player.dead	= False
 	if (player.movesdepleted == False) and (w >= 3):
 		delay(1)
-		player.hp	= player.startinghp
+		player.hp	= player.initialhp
 		flourish()
 		print("")
 		print("You have entered .. a zone")
@@ -252,7 +360,7 @@ def playerDead():
 		print(" ")
 		delay(1)
 		print("I.... The Ancient Wizard")
-		print(f"will restore your hit-points to {player.startinghp}")
+		print(f"will restore your hit-points to {player.initialhp}")
 		print("and .... You have one more")
 		print("chance in the Dungeon.")
 		print(" ")
@@ -463,7 +571,7 @@ def findVial():
 		print("It contains . . . a milky liquid.")
 		Print("Would like a drink?")
 		d	= input("Enter (Y)es or (N)o: ")
-		dl	= int(rnd()*6+)
+		dl	= int(rnd()*6+1)
 		if d.upper() == 'N':
 			print("You think maybe drinking strange 'milky' liquids")
 			print("in strange vials lying on the ground is a bad ")
@@ -487,7 +595,7 @@ def findVial():
 				print("You feel a little funny. . . ")
 				delay(2)
 				print("\nIt was a black magic potion. . .")
-				print(f"Which decreased your hit-points by {h}.')
+				print(f"Which decreased your hit-points by {h}.")
 				if player.dead:
 					print("It kind of killed you.")
 		return
@@ -577,7 +685,7 @@ def hiddenCavern():
 	delay(2)
 	w = int(rnd()*4+1)
 	somethingJumps()
-	if (w == 1) and (player.hp < player.startinghp):
+	if (w == 1) and (player.hp < player.initialhp):
 		mrWizard()
 	elif (w == 2):
 		giantSpider()
@@ -587,7 +695,61 @@ def hiddenCavern():
 		darkWizard()
 	return
 
+def introTop():
+	cls()
+	showFile("./intro.txt")
+	delay(5)
+	delay(2)
+	print("\n")
+	print("The Dungeon of Dangeresque".center(40))
+	print("   For Python 3".center(40))
+	print("(c) 1980 by Howard Berenbon".center(40))
+	print("Adapted from Atari BASIC verson".center(40))
+	print("Of 'Dunger of Danger'".center(40))
+	print(" ")
+	print("A Fantasy Game".center(40))
+	print("=-=-=-=-=-=-=-=-=-=-".center(40))
+	print("You will be teleported to...")
+	print(" ")
+	return
 
+def introMiddle():
+	print("The...")
+	showFile("./DungeonOfDangeresque-Logo.txt")
+	print(" ")
+	return
+
+def introBottom():
+	print(f'You carry a magic sword and {player.gold} gold pieces with you.')
+	print(f'Your hit-point value is {player.hp}.')
+	print("If it reaches zero, you will die . . . So be careful!")
+	delay(1)
+	print(f'{player.name} . . . You are on your way.')
+	delay(3)
+#	cls()
+	print("\n\n\n")
+	print("You have arrived at . . . ")
+	print(f"The Dungeon of Danger . . . Level {currentlevel}")
+	print(" ")
+	print("You will encounter monsters and thieves and . . . gold.")
+	print("Good luck!")
+	delay(3)
+	return
+
+def showCommands():
+	if debug:
+		print(f"Player X = {player.x}   Player Y = {player.y}  Room Type = {inroom} Level = {currentlevel}")
+		print(f"Monsters Killed (level)= {player.monsterskilled}  Monsters Killed (total) = {player.totalkills}")
+		print(f"Has Key = {player.haskey}  Has Map = {player.hasmap}")
+		print("\n")
+	print(f'Hit Points: {player.hp}  Gold: {player.gold}')
+	print(f'{player.name}, what is your action or move?')
+	print("(N)orth, (E)ast, (S)outh, (W)est")
+	print("(U)p, (M)ap")
+	if debug:
+		print("($)Give map & Key  (1) Show raw level")
+	return
+	
 ## Main Loop ##
 while gameloop:
 	if onload:
@@ -602,7 +764,8 @@ while gameloop:
 		currentlevel	= maxlevels
 		for i in range(1,maxlevels):
 			level[i].createMap()
-			level[i].map	= fillMap()
+#			level[i].map	= fillMap()
+			fillMap(i)
 		
 		player.x	= int(rnd()*8+1)
 		player.y	= int(rnd()*8+1)
@@ -615,8 +778,8 @@ while gameloop:
 		player.haskey			= False
 		player.movesleft		= 100
 		player.gold				= 500
-		player.startinghp		= 20+int(rnd()*15+1)
-		player.startinghp		= player.startinghp//difficulty
+		player.initialhp		= 20+int(rnd()*15+1)
+		player.initialhp		= player.initialhp//difficulty
 		targetkills				= int(rnd()*4+1)+4
 		player.monsterskilled	= 0
 		player.totalkills		= 0
@@ -631,37 +794,69 @@ while gameloop:
 #		cls()
 
 ##	Show what room player is in 
-		if (inroom == 1):		# Empty chamber
-			emptyChamber()		# 	Nothing happens here
-		elif (inroom == 2):		# Hidden Cavern
-			hiddenCavern()
-		elif (inroom == 3 ) or (inroom == 4):
-			occupiedCavern()
-		elif (inroom == 5):
-			thief()
-		elif (inroom == 6):
-			nsCorridor()
+	if (inroom == 1):		# Empty chamber
+		emptyChamber()		# 	Nothing happens here
+	elif (inroom == 2):		# Hidden Cavern
+		hiddenCavern()
+	elif (inroom == 3 ) or (inroom == 4):
+		occupiedCavern()
+	elif (inroom == 5):
+		thief()
+	elif (inroom == 6):
+		nsCorridor()
+	elif (inroom == 7):
+		ewCorridor()
+	elif (inroom == 8):
+		trapDoor()
+	elif (inroom == 9):
+		print("You are at a stairway")
+		print(". . . . . . .going up")
+	print(" ")
+	if player.dead:
+		playerDead()
+		continue
+		
+	showCommands()
+	pmove	= input("==> ")
+	pmove	= pmove.upper()
+		
+	if (pmove == 'N'):
+		if (player.y - 1 == 0):
+			atWall("North")
 		elif (inroom == 7):
-			ewCorridor()
-		elif (inroom == 8):
-			trapDoor()
-		elif (inroom == 9):
-			print("You are at a stairway")
-			print(". . . . . . .going up")
-		print(" ")
-		if player.dead:
-			playerDead()
-			continue
+			print("You are in an East-West passage.")
+		else:
+			player.move(player.x,player.y-1)
+	elif (pmove == 'E'):
+		if (player.x + 1) == 9:
+			atWall("East")
+		elif (inroom == 6):
+			print("You are in a North-South passage.")
+		else:
+			player.move(player.x+1,player.y)
+	elif (pmove == 'S'):
+		if (player.y + 1 == 9):
+			atWall("South")
+		elif (inroom ==7):
+			print("You are in an East-West passage.")
+		else:
+			player.move(player.x,player.y+1)
+	elif (pmove == 'W'):
+		if (player.x - 1 == 0):
+			atWall("West")
+		elif (inroom == 6):
+			print("You are in a North-South passage.")
+		else:
+			player.move(player.x-1,player.y)
 		
-		showCommands()
-		pmove	= input("==> ")
-		pmove	= pmove.upper()
-		
-		if (pmove == 'N'):
-			if (player.y - 1 == 0):
-				atWall("North")
-			elif (inroom == 7)
-				print("You are in a North-South passage.")
-			else:
-				player.move(player.x,player.y-1)
+	elif (pmove == 'U'):
+		goUpstairs()
+	elif (pmove == 'M'):
+		showMap()
+	elif (pmove == 'Q'):
+		quit()
+	print(" ")
+	inroom = level[currentlevel].roomContents(player.x,player.y)
+	player.turnEnd()
+	delay(2)
 		
--- dodfun.py
+++ dodfun.py
@@ -1,6 +1,12 @@
-# dodfun.py
-# Functions for Dungeons of Doom
-# that are helpful.
+####           The Dungeon of Dangeresque              ####   
+####    (c) 1980 Original by Howard Berenbon           ####
+#### Transcribed from:                                 ####
+#### Mostly BASIC: Applications for your Atari, Book 2 ####
+#### Published by Howard W. Sams & Co., Inc            ####
+#### Indianapolis, Indiana 1983                        ####
+####             Python Version 1.00                   ####
+####         Various Function -- dodfun,py             ####
+
 from os import system, name
 from random import random, seed
 from time import sleep
@@ -29,6 +35,198 @@ def showFile(filename):
 	file.close()
 	print(msg)
 	return
+
+def getDifficulty():
+	d	= 0
+	while (d < 1) or (d > 2):
+		print("Difficulty Level: 1 = Moderate, 2 = Hard")
+		n	= input("Enter difficulty level > ")
+		d	= int(n)
+	return(d)
+
+def getLevels(d):
+	numlevels	= d*10
+	return(numlevels)
+
+def getIndex(x,y):
+	i	=	x+(columns*y)
+	return(i)
+
+def getName():
+	n	=	input("Enter your character's name > ")
+	o	=	str(n)
+	return(o)
+	
+def flourish():				# This needs to be re-done
+	for aa in range(1,301):
+		print("*        %",end="")
+	delay(2)
+	print("\n")
+#	cls()
+	return
+	
+def createGrid(x,y):
+	grid=[]
+	for i in range(0,x*y):
+		grid.append(i)
+	return(grid)
+
+def fillArray(col,row):
+	grid	=	createGrid(col,row)
+	n		=	0
+	for y in range(1,col):
+		for x in range(1,row):
+			i	= getIndex(x,y)
+			grid[i]	= int(rnd()*7+1)
+	
+	h	=	int(rnd()*3+1)
+	for n in range(1,h+1):
+		x	=	int(rnd()*col+1)
+		y	=	int(rnd()*row+1)
+		i	=	getIndex(x,y)
+		grid[i]	=	8
+	
+	s	=	int(rnd()*4+1)+2
+	for n in range(1,s+1):
+		x	=	int(rnd()*8+1)
+		y	=	int(rnd()*8+1)
+		i	=	getIndex(x,y)
+		grid[i]	=	9
+	return(grid)
+
+def fillMap():
+	map = []
+	n	= 0
+	for i in range(0,9*9):
+		map.append(i)
+	
+	for y in range(1,9):
+		for x in range(1,9):
+			i = x+(9*y)
+			map[i] = int(rnd()*7+1)
+	h	= int(rnd()*3+1)
+	for n in range(1,h+1):
+		x	= int(rnd()*8)
+		y	= int(rnd()*8)
+		i	= x+(9*y)
+		map[i]	= 8
+	s	= int(rnd()*4+1)+2
+	for n in range(1,s+1):
+		x	= int(rnd()*8+1)
+		y	= int(rnd()*8+1)
+		i	= x+(9*y)
+		map[i]	= 9
+	return(map)
+	
+def monsterSetup():
+# Change this to suppport 3 levels of monsters
+	info=[[],[],[]]
+	info[0].append(0)
+	info[1].append(0)
+	info[2].append(0)
+	monster=['Large Dragon','Hideous Ghoul','Lizard Man','Manticore','Purple Worm','Deadly Cobra',
+			'Mad Elf','Clay Man','Hairy Beast','Mad Dwarf','Zombie','Berserker','Giant Scorpion',
+			'Giant Cockroach','Doppleganger','Giant Fire Beetle','Giant Ant','Giant Tick',
+			'Mummy','Nasty Orc','Skeleton','Troll','Goblin','Vampire Bat','Creeping Blob',
+			'Mad Dog','Large Spider','Black Cat','Man Eating Plant','Hydra','Gelatinous Cube',
+			'Giant Centipede','Giant Rat','Shadow']
+	monsterhp=[6,5,4,6,6,5,5,4,5,4,4,5,6,4,5,1,1,2,3,2,1,3,3,3,3,2,3,2,1,3,2,1,2,2]
+	monsterhm=[12,10,8,12,12,10,10,8,10,8,8,10,12,8,10,2,2,4,6,4,2,6,6,6,6,4,6,4,2,6,4,2,4,4]
+	monsterName=" "
+	monsterHitPower=0
+	monsterStrength=0
+	for loop in range(0,34):
+		info[0].append(monster[loop])
+		info[1].append(monsterhp[loop])
+		info[2].append(monsterhm[loop])
+	return(info)
+
+## Player Rating ##
+## Name - Score - Killed By - Rating
+def getRating(r):
+	rating = player.name+" - "+str(r)+" - " + player.killedby + " - "
+	if (r <= -400):
+		rate = "Incompetent serf"
+	elif (r <= -100):
+		rate = "Weakling"
+	elif (r < 0):
+		rate = "Apprentice"
+	elif (r < 100):
+		rate = "Halfling"
+	elif (r < 200):
+		rate = "Foot soldier"
+	elif (r < 600):
+		rate = "Warrior"
+	elif (r < 900):
+		rate = "Great Warrior"
+	elif (r < 1500):
+		rate = "Swordsman"
+	elif (r < 2500):
+		rate = "Magic Swordsman"
+	elif (r >= 2500):
+		rate = "Dungeon Master"
+	rating = rating + rate
+	return(rating)
+
+def showMap():		# Line 1570 & 1990
+	cls()
+	if (player.hasmap == False):
+		print("You pat your pockets looking for your map")
+		print("before you remember you don't have one.")
+		delay(1)
+	else:
+		print(f"The Dungeon of Danger Map: Level {currentlevel}")
+		print(" ")
+		for q in range(1,9):
+			for n in range(1,9):
+				if (player.x == n) and (player.y == q):
+					print("Pl ",end=" ")
+					continue
+				else:
+					idx = level[currentlevel].getIndex(n,q)
+					s1	= level[currentlevel].map[idx]
+					if s1 == 1:
+						print("O  ",end=" ")
+					elif s1 == 2:
+						print("C  ",end=" ")
+					elif s1 == 3:
+						print("M  ",end=" ")
+					elif s1 == 4:
+						print("M  ",end=" ")
+					elif s1 == 5:
+						print("?  ",end=" ")
+					elif s1 == 6:
+						print("NS ",end=" ")
+					elif s1 == 7:
+						print("EW ",end=" ")
+					elif s1 == 8:
+						print("?  ",end=" ")
+					elif s1 == 9:
+						print("UP ",end=" ")
+			print("\n")
+#	if debug:
+#		level[currentlevel].showMap()
+
+	dummy = input("~~Press Enter to Continue~~")
+	return	
+	
+def newGame():
+	loop = True
+	
+	while loop:
+		print("Game Over")
+		print("Would you like to play again?")
+		f = input("Enter (Y)es or (N)o :> ")
+		if f.upper() == 'Y' or f.upper() == 'N':
+			loop = False
+	return(f.upper())
+
+def atWall(dir):
+	print(f'You are at the {dir} wall')
+	print("You cannot pass through")
+	print(" ")
+	print("Try another direction?")
+	return
 	
 class playerObject:
 	name			= 'name'
@@ -46,8 +244,9 @@ class playerObject:
 	hasmap			= False
 	dead			= False
 	movesdepleted	= False
-	startinghp		= 0
+	initialhp		= 0
 	healthturn		= 0
+	onlevel			= 0
 	deathreason		= " "
 	
 	def decHP(x):
@@ -65,12 +264,12 @@ class playerObject:
 		playerObject.turnsleft	= playerObject.turnsleft - 1
 		if playerObject.turnsleft == 0:
 			playerObject.movesdepleted = True
-		if playerObject.hp < playerObject.startinghp:
+		if playerObject.hp < playerObject.initialhp:
 			playerObject.healthturn = playerObject.healthturn + 1
 			if playerObject.healthturn > 5:
 				playerObject.hp = playerObject.hp + int(rnd()*5)+1
-				if playerObject.hp > playerObject.startinghp:
-					playerObject.hp = playerObject.startinghp
+				if playerObject.hp > playerObject.initialhp:
+					playerObject.hp = playerObject.initialhp
 					print("You feel like your old self again.")
 				else:
 					playerObject.healthturn = 0
@@ -100,6 +299,39 @@ class playerObject:
 		else:
 			return True
 	
+	def upLevel():
+		playerObject.haskey = False
+		print("Your Enchanted Key melts in the lock!")
+		playerObject.hasmap = False
+		print("Your map turns to dust!")
+		if playerObject.hp < initialHP:
+			playerObject.hp = initialHP
+			print("You feel stronger. . . . . ")
+			delay(1)
+			print(f"Your hit-points are restored to {initialHP}")
+			print()
+		playerObject.monsterskilled	= 0
+		print(f"You have gotten to level {currentlevel}")
+		delay(2)
+		return
+	
+	def reset(d):
+		playerObject.x				=	int(rnd()*8+1)
+		playerObject.y				=	int(rnd()*8+1)
+		playerObject.startingHP		=	20+(int(rnd()*15)+1)//d
+		
+		playerObject.movesdepleted	=	False
+		playerObject.hasmap			= 	False
+		playerObject.haskey			=	False
+		
+		playerObject.movesleft		=	100
+		playerObject.gold			=	500
+		playerObject.monsterskilled	=	0
+		playerObject.totalkills		=	0
+		playerObject.hp				=	playerObject.startingHP
+		return
+
+		
 class monsterObject:
 	name	= 'monster'
 	hp		= 1
@@ -127,75 +359,75 @@ class levelObject:
 	def __init__(self):
 		return
 		
-	def createMap(self):
-		for i in range(0,self.mx*self.my):
-			self.map.append(i)
+	def createMap():
+		for i in range(0,levelObject.mx*levelObject.my):
+			levelObject.map.append(i)
 		return
 	
-	def getIndex(self,x,y):
-		i=x+(self.mx*y)
+	def getIndex(x,y):
+		i=x+(levelObject.mx*y)
 		return(i)
 
-	def getX(self,i):
-		x=i%self.mx
+	def getX(i):
+		x=i%levelObject.mx
 		return(x)
 
-	def getY(self,i):
-		y=i//self.mx
+	def getY(i):
+		y=i//levelObject.mx
 		return(y)
 
-	def getXY(self,i):
-		x=i%self.mx
-		y=i//self.mx
+	def getXY(i):
+		x=i%levelObject.mx
+		y=i//levelObject.mx
 		xy=[x,y]
 		return(xy)
 	
-	def showMap(self):
+	def showMap():
 		index=0
-		for ol in range(0,self.mx):
-			for il in range(0,self.mx):
-				print('{0:2d}'.format(self.map[index]),end=" ")
+		for ol in range(0,levelObject.mx):
+			for il in range(0,levelObject.mx):
+				print('{0:2d}'.format(levelObject.map[index]),end=" ")
 				index=index+1
 			print("\n")
 		return
 
-	def roomContents(self,x,y):
+	def roomContents(x,y):
 		index = 0
-		index = self.getIndex(x,y)
-		contents = self.map[index]
+		index = levelObject.getIndex(x,y)
+		contents = levelObject.map[index]
 		return(contents)
 		
-	def setMap(self,x,y,v):
-		index = self.getIndex(x,y)
-		self.map[index] = v
+	def setMap(x,y,v):
+		index = levelObject.getIndex(x,y)
+		levelObject.map[index] = v
 		return
 
-	def setMapIdx(self,idx,value):
-		self.map[idx]	= value
+	def setMapIdx(idx,value):
+		levelObject.map[idx]	= value
 		return
 		
-	def fillMap(self):
+	def fillMap():
 		seed()
 		n	= 0
-		for y in range(1,self.mx):
-			for x in range(1,self.my):
-				i = self.getIndex(x,y)
-				self.map[i] = int(rnd()*7+1)
+		for y in range(1,levelObject.mx):
+			for x in range(1,levelObject.my):
+				i = levelObject.getIndex(x,y)
+				levelObject.map[i] = int(rnd()*7+1)
 		
 		h	= int(rnd()*3+1)
 		
 		for n in range(1,h+1):
-			x	= int(rnd()*self.mx)
-			y	= int(rnd()*self.my)
-			i	= self.getIndex(x,y)
-			self.map[i]	= 8
+			x	= int(rnd()*levelObject.mx)
+			y	= int(rnd()*levelObject.my)
+			i	= levelObject.getIndex(x,y)
+			levelObject.map[i]	= 8
 		
 		s	= int(rnd()*4+1)+2
 		
 		for n in range(1,s+1):
 			x	= int(rnd()*8+1)
 			y	= int(rnd()*8+1)
-			i	= self.getIndex(x,y)
-			self.map[i]	= 9
+			i	= levelObject.getIndex(x,y)
+			levelObject.map[i]	= 9
 		return
 
